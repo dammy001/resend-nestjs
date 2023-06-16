2b416b9054e4f812097bddf5610f8cd582adee0a
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-06-16 20:45:17 +0100

    chore: add example

--- src/resend.service.ts
+++ src/resend.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
+import { Resend } from 'resend'
+import { RESEND_MODULE_OPTIONS } from './resend.constant'
+import type { ResendModuleOptions } from './resend.interface'
 
 @Injectable()
-export class ResendService {}
+export class ResendService {
+  constructor(
+    @Inject(RESEND_MODULE_OPTIONS)
+    private readonly options: ResendModuleOptions,
+  ) {}
+
+  public resend(): Resend {
+    return new Resend(this.options.key)
+  }
+}
